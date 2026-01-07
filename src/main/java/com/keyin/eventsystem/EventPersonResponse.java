a832114f1633ef88846e9a6f576340a2812dc721
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2026-01-07 10:48:05 -0330

    add EventPersonResponse, EventTest, and Person classes

---
+++ src/main/java/com/keyin/eventsystem/EventPersonResponse.java
@@ -0,0 +1,6 @@
+package com.keyin.eventsystem;
+
+public class EventPersonResponse {
+    private String eventName;
+    private String personName;
+}
---
+++ src/test/java/com/keyin/eventsystem/EventTest.java
@@ -0,0 +1,6 @@
+package com.keyin.eventsystem;
+
+public class EventTest {
+
+    // Event Tests would go here
+}
