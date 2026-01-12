68a6b225ab6b00a38b048da1f6d9818e4b5813db
Jordan Kelloway <106637327+JKells99@users.noreply.github.com>
2026-01-12 15:02:53 -0330

    add Animal, Dog, AnimalDAO, and AnimalTesting classes

---
+++ src/main/java/com/keyin/javareview/AnimalDAO.java
@@ -0,0 +1,12 @@
+package com.keyin.javareview;
+
+public class AnimalDAO {
+
+
+    public void addAnimal(Animal animal){
+        System.out.println("Animal has been added " + animal.getName());
+
+    }
+
+
+}
---
+++ src/main/java/com/keyin/javareview/RestRepo.java
@@ -0,0 +1,5 @@
+package com.keyin.javareview;
+
+public interface RestRepo {
+
+}
